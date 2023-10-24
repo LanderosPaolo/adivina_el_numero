10f2f04f4c69ceedb253e4529651a72af919dc86
Paolo Landeros <landerospaolo@gmail.com>
2023-10-23 22:01:39 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# adivina_el_numero
+# Juego Adivina el Número en Python
+
+Este es un juego simple de adivinanza de números implementado en Python. El juego elige un número aleatorio entre 1 y 10 y te proporciona pistas para ayudarte a adivinarlo en un número limitado de intentos.
+
+## Instrucciones de Juego
+
+1. Ejecuta el programa.
+2. Ingresa tu nombre cuando se te solicite.
+3. El programa elegirá un número aleatorio entre 1 y 10.
+4. Recibirás pistas sobre si el número es par o impar.
+5. Tienes tres intentos para adivinar el número correcto.
+6. Ingresa un número entre 1 y 10 como tu suposición.
+7. El juego te proporcionará pistas si tu suposición es mayor o menor que el número seleccionado.
+8. Si adivinas el número, ¡ganarás el juego y recibirás felicitaciones!
+9. Si te quedas sin intentos, el juego revelará el número correcto.
+
+## Cómo Ejecutar el Juego
+
+1. Clona este repositorio en tu computadora.
+2. Abre una terminal en el directorio donde se encuentra el código.
+3. Ejecuta el juego
+4. Sigue las instrucciones en la terminal para jugar.
--- adivina.py
+++ adivina.py
@@ -2,8 +2,8 @@
 from random import randint
 
 # Se crea la variable que guardara el nombre del usuario
-nombre = input('- Ingresa tú nombre: ')
-# El número elegido sera de manera aleatoria entre 1 y 10
+nombre = input('- Ingresa tu nombre: ')
+# El número elegido será de manera aleatoria entre 1 y 10
 numero = randint(1,10)
 # Determinamos si el número aleatorio que ha salido es par o impar
 if(numero % 2 == 0):
@@ -15,45 +15,45 @@ intentos = 3
 
 # Le preguntamos al usuario si quiere comenzar el juego
 jugar = input(f"""- Hola {nombre}.
-estoy pensando en un número entre el 1 y el 10
-te animas a jugar? si/no: """).strip().lower()
+- Estoy pensando en un número entre el 1 y el 10
+- ¿Te animas a jugar? si/no: """).strip().lower()
 # Se crea un bucle en caso de que el usuario responda algo distinto a si/no
 while jugar not in ["si", "no"]:
     print("- Necesito que respondas SI quieres jugar o NO")
-    jugar = input(f"- {nombre}, quieres jugar? si/no ")
+    jugar = input(f"- {nombre}, Quieres jugar? si/no ")
 
 if(jugar == "si"):
     print(numero)
     while intentos > 0:
         # Se le pide al jugador que adivine el número
-        numero_jugador = int(input('- Entre los números 1 y 10, cual crees que tengo?: '))
+        numero_jugador = int(input('- Entre los números 1 y 10. Cuál crees que tengo?: '))
         # Se crea una condicional en caso de que el usuario escoja un número distinto a los solicitados
         if(numero_jugador > 10 or numero_jugador < 1):
             print(f"- {nombre}, solo se permite un número entre 1 y 10")
-        # en caso de ser mayor o menor al número de la computadora, nos proporciona pistas
+        # En caso de ser mayor o menor al número de la computadora, nos proporciona pistas
         elif(numero_jugador < numero):
             intentos -= 1
             print(f"""- Incorrecto!!, intenta una vez más!
--(pista 1): El número que has escogido es menor.
--(Pista 2): El número que tengo es {pista}
+- (pista 1): El número que has escogido es menor.
+- (Pista 2): El número que tengo es {pista}
 - Intentos restantes: {intentos}""")
         elif(numero_jugador > numero):
             intentos -= 1
             print(f"""- Incorrecto!!, intenta una vez más!
--(Pista 1): El número que has escogido es mayor. 
--(Pista 2): El número que tengo es {pista}
+- (Pista 1): El número que has escogido es mayor. 
+- (Pista 2): El número que tengo es {pista}
 - Intentos restantes: {intentos}""")
         # En caso de acertar, nos felicita
         else:
             print(f"- Felicitaciones {nombre}!!, has acertado el número!!!")
             break
-    # En caso de quedar sin intentos nos da un aviso y nos revela el número que tenia
+    # En caso de quedar sin intentos nos da un aviso y nos revela el número que tenía
     if(intentos == 0):
         print(f"""- {nombre}, te has quedado sin intentos!
 - El número que tenía era el {numero}""")
-    # Si aun quedan intentos, despues de contestar nos avisa cuantos nos quedan
+    # Si aún quedan intentos, despues de contestar nos avisa cuantos nos quedan
     else:
         print(f"---> Tus intentos restantes: {intentos - 1} <---")
 # En caso de no querer jugar, el programa termina
 else:
-    print("- Que pena, vuelve luego")
+    print("- Qué pena, vuelve luego")
